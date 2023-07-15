f044af1bfb8cb88a3887680cfed4497aa2c982e2
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-07-15 15:36:18 +0000

    files added

--- markdown2html.py
+++ markdown2html.py
@@ -1,17 +1,29 @@
+#!/usr/bin/python3
+''' Markdown to HTML '''
 import sys
-import os.path
+from os import path
 
-if len(sys.argv) < 3:
-    sys.stderr.write("Usage: ./markdown2html.py <input_file> <output_file>\n")
-    sys.exit(1)
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
-
-if not os.path.isfile(input_file):
-    sys.stderr.write("Missing " + input_file + "\n")
-    sys.exit(1)
-
-# Process the Markdown file and convert it to HTML
-
-sys.exit(0)
+if __name__ == "__main__":
+    '''the number of arguments is less than 2'''
+    if len(sys.argv) != 3:
+        print('Usage: ./markdown2html.py README.md README.html' ,file=sys.stderr)
+        exit(1)
+    '''Markdown file doesn’t exist'''
+    if not path.exists(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        exit(1)
+    '''Headings Markdown'''
+    with open(sys.argv[1], 'r') as read_file:
+        line_list = []
+        for lines in read_file.readlines():
+            cout_cra = 0
+            for line in lines:
+                for car in range(len(line)):
+                    if line[car] == '#':
+                        cout_cra += 1
+            lines = lines.rstrip('\r\n')
+            line_list.append("<h{}>{}</h{}>".format(cout_cra, lines.replace('#',''), cout_cra))
+        with open(sys.argv[2], 'a') as write_file:
+            for line in line_list:
+                write_file.write('{}\n'.format(line))
